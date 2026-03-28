2037128736c461eeb4675284aa28cccbaff2a5bd
Daniel Barutov <barutovdg@gmail.com>
2026-03-28 09:12:41 +0300

    refix format ruff

--- app/migrations/versions/771abab20206_init.py
+++ app/migrations/versions/771abab20206_init.py
@@ -8,9 +8,6 @@ Create Date: 2026-03-28 08:45:55.770383
 
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision: str = "771abab20206"
