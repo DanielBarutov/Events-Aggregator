a030d4635048692a2374f043a239b208c92fc801
Daniel Barutov <barutovdg@gmail.com>
2026-04-13 13:50:13 +0300

    fix get_outbox select

--- src/infrastructure/repository/outbox.py
+++ src/infrastructure/repository/outbox.py
@@ -13,7 +13,7 @@ class OutboxRepository:
     async def get_outbox(self) -> list[OutboxEntity] | None:
         try:
             data = await self.session.execute(
-                select(Outbox).where(OutboxStatus == "await")
+                select(Outbox).where(Outbox.status == "await")
             )
             outboxes = data.scalars().all()
             return (
