46cc013f5d4ff734026f2071882cf3d6dc34bb41
Daniel Barutov <barutovdg@gmail.com>
2026-04-14 12:39:02 +0300

    fix sleep task outbox

--- src/application/service/outbox_worker.py
+++ src/application/service/outbox_worker.py
@@ -21,7 +21,7 @@ async def run_outbox_loop(build_usecase: Callable[[], Awaitable[OutboxUsecase]])
             try:
                 usecase = await build_usecase(session)
                 await usecase.execute()
-                await asyncio.sleep(20)
+                await asyncio.sleep(10)
             except Exception as e:
                 logger.exception("Возникла ошибка при работе outbox_worker", exc_info=e)
                 await asyncio.sleep(5)
