c025db2acc03204efeb0941d323dcabf24b8f5a9
Daniel Barutov <barutovdg@gmail.com>
2026-04-14 10:49:32 +0300

    added sentry test

--- src/presentation/api/v1/health.py
+++ src/presentation/api/v1/health.py
@@ -6,3 +6,13 @@ router = APIRouter(tags=["health"])
 @router.get("/health")
 async def health():
     return {"status": "ok"}
+
+
+@router.get("/sentry-test")
+async def sentry_test():
+    raise Exception("Test exception")
+
+
+@router.get("/sentry-test-2")
+async def sentry_test_2():
+    return 1 / 0
