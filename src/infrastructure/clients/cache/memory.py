e026b0c935697ad368f5a21dd313feeb0e18a850
Daniel Barutov <barutovdg@gmail.com>
2026-04-21 12:54:29 +0300

    Remove redundant parentheses from class definition

--- src/infrastructure/clients/cache/memory.py
+++ src/infrastructure/clients/cache/memory.py
@@ -9,7 +9,7 @@ from src.domain.exceptions import AppError, CacheError
 logger = logging.getLogger(__name__)
 
 
-class MemoryCache():
+class MemoryCache:
     def __init__(self):
         self._storage: dict[str, tuple[typing.Any, float]] = {}
 
